774fdc834ac80810bb0a917e16d0c6fc2d1276d5
HakiEBIBI <haki.ebibi@jobtrek.ch>
2024-12-16 16:26:41 +0100

    feat: added a input bar and when we click on an button we got a list with what we write on the input bar

--- src/main.ts
+++ src/main.ts
@@ -1,3 +1,20 @@
 import './style.css'
 
 console.log('Hello from typescript')
+
+const inputBox: Element | null = document.querySelector('#todo-input')
+const listBox: Element | null = document.querySelector('#todo-element')
+const buttonBox: Element | null = document.querySelector('#add-todo-button')
+
+if (buttonBox && inputBox && listBox) {
+  buttonBox.addEventListener('click', () => {
+    const todoText = (inputBox as HTMLInputElement).value
+
+    if (todoText.trim() !== '') {
+      const li = document.createElement('li')
+      li.textContent = todoText
+      listBox.appendChild(li)
+      ;(inputBox as HTMLInputElement).value = ''
+    }
+  })
+}
