177075ff42e052ff0e9702c3a0a26a93675887fa
HakiEBIBI <haki.ebibi@jobtrek.ch>
2024-12-17 08:28:38 +0100

    refactor: use type generic instead of as

--- src/main.ts
+++ src/main.ts
@@ -2,19 +2,19 @@ import './style.css'
 
 console.log('Hello from typescript')
 
-const inputBox: Element | null = document.querySelector('#todo-input')
-const listBox: Element | null = document.querySelector('#todo-element')
-const buttonBox: Element | null = document.querySelector('#add-todo-button')
+const inputBox = document.querySelector<HTMLInputElement>('#todo-input')
+const listBox = document.querySelector<HTMLUListElement>('#todo-element')
+const buttonBox = document.querySelector<HTMLButtonElement>('#add-todo-button')
 
 if (buttonBox && inputBox && listBox) {
   buttonBox.addEventListener('click', () => {
-    const todoText = (inputBox as HTMLInputElement).value
+    const todoText = inputBox.value
 
     if (todoText.trim() !== '') {
       const li = document.createElement('li')
       li.textContent = todoText
       listBox.appendChild(li)
-      ;(inputBox as HTMLInputElement).value = ''
+      inputBox.value = ''
     }
   })
 }
