4a835ab93d503f508fc730a27d8b837bc72822a0
HakiEBIBI <haki.ebibi@jobtrek.ch>
2024-12-17 12:42:58 +0100

    feat: added a button and when we clicked it we delete an todo

--- src/main.ts
+++ src/main.ts
@@ -10,11 +10,21 @@ if (buttonBox && inputBox && listBox) {
   buttonBox.addEventListener('click', () => {
     const todoText = inputBox.value
 
+    const button = document.createElement('button')
+    button.addEventListener('click', () => {})
+
     if (todoText.trim() !== '') {
       const li = document.createElement('li')
       li.textContent = todoText
+      button.textContent = 'delete'
       listBox.appendChild(li)
+      listBox.appendChild(button)
       inputBox.value = ''
+
+      button.addEventListener('click', () => {
+        li.remove()
+        button.remove()
+      })
     }
   })
 }
