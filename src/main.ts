733625e0e782963e911e57dea84e3df0d1f4d384
HakiEBIBI <haki.ebibi@jobtrek.ch>
2025-01-14 13:05:29 +0100

    feat: added a error message if a date is not valid, if the todo are more than 200 characters and if the text have nothing close #6

--- src/main.ts
+++ src/main.ts
@@ -6,14 +6,33 @@ const inputBox = document.querySelector<HTMLInputElement>('#todo-input')
 const listBox = document.querySelector<HTMLUListElement>('#todo-element')
 const buttonBox = document.querySelector<HTMLButtonElement>('#add-todo-button')
 const datetime = document.querySelector<HTMLInputElement>('#datetime-input')
+const errorBox = document.querySelector<HTMLParagraphElement>(
+  '#todo-creation-error',
+)
 
-const todos = JSON.parse(localStorage.getItem('todos') || '[]')
+if (inputBox && listBox && buttonBox && datetime && errorBox) {
+  const todos = JSON.parse(localStorage.getItem('todos') || '[]')
 
-const saveTodos = () => {
-  localStorage.setItem('todos', JSON.stringify(todos))
-}
+  const saveTodos = () => {
+    localStorage.setItem('todos', JSON.stringify(todos))
+  }
 
-if (buttonBox && inputBox && listBox && datetime) {
+  const handleCheckboxChange = (todo: { completed: boolean }) => {
+    return (event: Event) => {
+      const checkbox = event.target as HTMLInputElement
+      todo.completed = checkbox.checked
+      saveTodos()
+    }
+  }
+
+  const handleDeleteClick = (index: number, li: HTMLLIElement) => {
+    return () => {
+      todos.splice(index, 1)
+      saveTodos()
+      li.remove()
+    }
+  }
+
   const createTodoElement = (
     todo: { text: string; completed: boolean; date: string },
     index: number,
@@ -31,38 +50,62 @@ if (buttonBox && inputBox && listBox && datetime) {
     li.appendChild(button)
     listBox.appendChild(li)
 
-    checkbox.addEventListener('change', () => {
-      todo.completed = checkbox.checked
-      saveTodos()
-    })
+    checkbox.addEventListener('change', handleCheckboxChange(todo))
+    button.addEventListener('click', handleDeleteClick(index, li))
+  }
 
-    button.addEventListener('click', () => {
-      todos.splice(index, 1)
+  const handleAddTodo = () => {
+    const todoText = inputBox.value.trim() || ''
+    const savedDate = datetime.value.trim() || 'no due date'
+
+    // check if text length are bigger than 200
+    if (todoText.length > 200) {
+      errorBox.innerText = 'todo cant take more than 200 characters'
+      inputBox.value = ''
+      return
+    }
+
+    if (todoText === '') {
+      errorBox.innerText = 'please enter something'
+      inputBox.value = ''
+      return
+    }
+
+    if (savedDate) {
+      const parsedDate = new Date(savedDate)
+      if (Number.isNaN(parsedDate.getTime())) {
+        errorBox.innerText = 'pls enter a valid date'
+        datetime.value = ''
+        return
+      }
+    }
+
+    // put the user input in a li and clear input box and date time
+    if (todoText !== '') {
+      const newTodo = {
+        text: todoText,
+        completed: false,
+        date: savedDate || 'no due date',
+      }
+      todos.push(newTodo)
       saveTodos()
-      li.remove()
-    })
+      createTodoElement(newTodo, todos.length - 1)
+      if (inputBox) inputBox.value = ''
+      if (datetime) datetime.value = ''
+      errorBox.innerText = ''
+    }
+  }
+
+  const handleKeyPress = (event: KeyboardEvent) => {
+    if (event.key === 'Enter') {
+      buttonBox.click()
+    }
   }
 
   for (const [index, todo] of todos.entries()) {
     createTodoElement(todo, index)
   }
 
-  buttonBox.addEventListener('click', () => {
-    const todoText = inputBox.value.trim()
-    const saveddate = datetime.value.trim()
-    if (todoText !== '') {
-      const newTodo = { text: todoText, completed: false, date: saveddate }
-      todos.push(newTodo)
-      saveTodos()
-      createTodoElement(newTodo, todos.length - 1)
-      inputBox.value = ''
-      datetime.value = ''
-    }
-  })
-
-  inputBox.addEventListener('keydown', (event: KeyboardEvent) => {
-    if (event.key === 'Enter') {
-      buttonBox.click()
-    }
-  })
+  buttonBox.addEventListener('click', handleAddTodo)
+  inputBox.addEventListener('keydown', handleKeyPress)
 }
